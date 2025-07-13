7fcd717189bdc36bcde242698c377cf212ebc110
Taki Takanashi <gustavo.almeidafn@gmail.com>
2025-07-13 17:04:57 -0300

    remove-video-channel: keep mode

--- remove-video-channel/remove-video-channel.py
+++ remove-video-channel/remove-video-channel.py
@@ -5,6 +5,7 @@ from pathlib import Path
 parser = argparse.ArgumentParser()
 parser.add_argument("--audio", "-a", action="append", default=[], type=int, help="Audio channels to remove")
 parser.add_argument("--subtitle", "-s", action="append", default=[], type=int, help="Subtitle channels to remove")
+parser.add_argument("--mode", "-m", choices=["remove", "keep"], default="remove", help="Whether to remove or keep specified channels")
 args = parser.parse_args()
 
 if len(args.audio) == 0 and len(args.subtitle) == 0:
@@ -16,17 +17,28 @@ Path("output").mkdir(exist_ok = True)
 allowedExtensions = { ".mkv", ".mp4" }
 for path in Path(".").glob("*"):
     if path.exists() and path.is_file() and path.suffix.lower() in allowedExtensions:
-        ffmpegArgs = [ "ffmpeg", "-i", str(path), "-map", "0", "-c", "copy", "-disposition:s:0", "default", f"output/{path.name}" ]
-        channels: list[str] = []
-        if len(args.audio) > 0:
+        ffmpegArgs = [ "ffmpeg", "-i", str(path), "-c", "copy", "-disposition:s:0", "default", f"output/{path.name}" ]
+        mapArgs: list[str] = []
+
+        if args.mode == "remove":
+            mapArgs += ["-map", "0"] # Keep everything
             for channel in args.audio:
-                channels.append("-map")
-                channels.append(f"-0:a:{channel}")
-
-        if len(args.subtitle) > 0:
+                mapArgs += ["-map", f"-0:a:{channel}"]
             for channel in args.subtitle:
-                channels.append("-map")
-                channels.append(f"-0:s:{channel}")
+                mapArgs += ["-map", f"-0:s:{channel}"]
+        elif args.mode == "keep":
+            mapArgs += ["-map", "0:v"] # Keep all video channels
+            if (len(args.audio) > 0):
+                for channel in args.audio:
+                    mapArgs += ["-map", f"0:a:{channel}"]
+            else:
+                mapArgs += ["-map", "0:a"] # Keep all audio if no channels are specified
 
-        ffmpegArgs[5:5] = channels
+            if (len(args.subtitle) > 0):
+                for channel in args.subtitle:
+                    mapArgs += ["-map", f"0:s:{channel}"]
+            else:
+                mapArgs += ["-map", "0:s"] # Keep all subtitles if no channels are specified
+
+        ffmpegArgs[3:3] = mapArgs
         subprocess.run(ffmpegArgs)
