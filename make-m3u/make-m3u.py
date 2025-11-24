5baaf19a587259d15a32db7eb932ac0fee5b4602
Taki Takanshi <gustavo.almeidafn@gmail.com>
2025-11-24 06:38:09 -0300

    New script: make-m3u

---
+++ make-m3u/make-m3u.py
@@ -0,0 +1,25 @@
+import argparse
+from pathlib import Path
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--name", "-n", default="", help="The playlist name. Defaults to current directory.")
+parser.add_argument("--overwrite", "-ow", action="store_true", help="Overwrite the playlist file if already exists.")
+parser.add_argument("--absolute", "-a", action="store_true", help="Write absolute paths instead of relative.")
+args = parser.parse_args()
+
+allowed_extensions = { ".mkv", ".mp4" }
+playlist_file = args.name if args.name else Path().cwd().stem
+if not playlist_file.endswith(".m3u"):
+    playlist_file += ".m3u"
+
+if (Path(playlist_file).exists() and not args.overwrite):
+    print(f"ERROR: Playlist file {playlist_file} already exists, if you with to overwrite the existing file, use --overwrite/-ow")
+    exit(1)
+
+print(f"> {playlist_file}")
+with open(playlist_file, 'w') as file:
+    for path in sorted(Path(".").iterdir()):
+        if path.suffix.lower() in allowed_extensions:
+            p = path.absolute() if args.absolute else path
+            print(f"  * {p}")
+            file.write(f"{p}\n")
